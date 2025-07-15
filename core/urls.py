f672dcad634e236917437f4b339f9a3a42f54270
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-07-15 17:02:51 -0300

    feat: update and delete routes created for cart

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('products/<int:pk>/', views.ProductAPIView.as_view(), name='detail_product'),
     path('categories/', views.CategoriesAPIView.as_view(), name='categories'),
     path('categories/<int:pk>/', views.CategoryAPIView.as_view(), name='detail_category'),
-    path('carts/', views.CartAPIView.as_view(), name='carts'),
-    path('carts/items/', views.CartItemAPIView.as_view(), name='cart_item'),
+    path('carts/', views.CartsAPIView.as_view(), name='carts'),
+    path('carts/<int:pk>/', views.CartAPIView.as_view(), name='detail_cart'),
+    path('carts/items/', views.CartItemsAPIView.as_view(), name='cart_item'),
+    path('carts/items/<int:pk>/', views.CartItemAPIView.as_view(), name='detail_cart_item'),
 ]
