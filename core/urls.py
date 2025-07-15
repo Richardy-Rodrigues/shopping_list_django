100abdf59310402e7a1fddec5c8852171ddfffaa
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-07-15 15:28:40 -0300

    chore: correct urls

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('users/<int:pk>/', views.UserAPIView.as_view(), name='detail_user'),
     path('products/', views.ProductsAPIView.as_view(), name='products'),
     path('products/<int:pk>/', views.ProductAPIView.as_view(), name='detail_product'),
+    path('categories/', views.CategoriesAPIView.as_view(), name='categories'),
+    path('categories/<int:pk>/', views.CategoryAPIView.as_view(), name='detail_category'),
+    path('carts/', views.CartAPIView.as_view(), name='carts'),
+    path('carts/items/', views.CartItemAPIView.as_view(), name='cart_item'),
 ]
